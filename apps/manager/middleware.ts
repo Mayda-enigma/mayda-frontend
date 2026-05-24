eafbd872192999321e9a6265d18bc56dcad7ae38
benassloun abdallah <benasslounabdallah@gmail.com>
2026-05-24 12:00:32 +0100

    Revert "fix: middleware allowing svgs, fix waiter logo size"
    This reverts commit ee4b11beccfebe2e0ecd30fe095d496de7024a74.

--- apps/manager/middleware.ts
+++ apps/manager/middleware.ts
@@ -20,5 +20,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:png|svg)$).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.png$).*)'],
 };
